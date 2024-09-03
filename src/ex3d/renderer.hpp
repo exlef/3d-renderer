5c65b2cae6131054bc329904f463847d7180a5ed
exlef <50216668+exlef@users.noreply.github.com>
2024-09-03 15:15:41 +0300

    add create window function

---
+++ src/ex3d/renderer.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "GL/glew.h"
+#define GL_SILENCE_DEPRECATION
+#include "GLFW/glfw3.h"
+#include <string>
+#include <iostream>
+
+namespace ex
+{
+    class renderer
+    {
+    private:
+        int m_screen_width = 0;
+        int m_screen_height = 0;
+        GLFWwindow* m_window = nullptr;
+
+    public:
+        int get_screen_width() const { return m_screen_width; }
+        int get_screen_height() const { return m_screen_height; }
+        GLFWwindow* window() const { return m_window; }
+
+        int create_window(int width, int height, const std::string& title)
+        {
+            #define SUCC 1
+            #define FAIL -1
+
+            // glfw: initialize and configure
+            // ------------------------------
+            glfwInit();
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+            // glfw window creation
+            // --------------------
+            m_screen_width = width;
+            m_screen_height = height;
+            m_window = glfwCreateWindow(m_screen_width, m_screen_height, title.c_str(), NULL, NULL);
+            if (m_window == NULL)
+            {
+                std::cerr << "Failed to create GLFW window" << std::endl;
+                glfwTerminate();
+                return FAIL;
+            }
+            // this function makes the context of specified window current on the calling thread.
+            glfwMakeContextCurrent(m_window);
+            // set vsyn on
+            glfwSwapInterval(1);
+            // set callback for window resize
+            // glfwSetFramebufferSizeCallback(m_window, framebuffer_size_callback);
+            // set callback for when mouse moves
+            // glfwSetCursorPosCallback(m_window, mouse_callback);
+            // set callback for mouse scroll wheel scrolls
+            // glfwSetScrollCallback(m_window, scroll_callback);
+
+            // tell GLFW to capture our mouse
+            glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+            if (glewInit() != GLEW_OK)
+            {
+                std::cerr << "Failed to initialize GLEW" << std::endl;
+                return FAIL;
+            }
+
+            return SUCC;
+        }
+    };
+} // namespace ex
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include "ex3d/renderer.hpp"
 
 int main()
 {
+    ex::renderer renderer;
+    renderer.create_window(800, 600, "test");
     std::cout << "hello" << std::endl;
 }
