f19e5fb8104c1faff89a6c7c46bc51153f3fd386
exlef <50216668+exlef@users.noreply.github.com>
2024-09-10 01:06:44 +0300

    fix model is passing by value

--- src/ex3d/app.hpp
+++ src/ex3d/app.hpp
@@ -232,7 +232,7 @@ namespace ex
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         }
 
-        void draw(Model model)
+        void draw(Model& model)
         {
             for (unsigned int i = 0; i < model.meshes.size(); i++)
                 model.meshes[i].Draw();
