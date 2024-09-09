d0598d622ac3cc078505be4c3fb5ef7d6535ad6f
exlef <50216668+exlef@users.noreply.github.com>
2024-09-10 01:07:30 +0300

    Update model.hpp

--- src/ex3d/model.hpp
+++ src/ex3d/model.hpp
@@ -20,7 +20,7 @@ namespace ex
 
         Model() = delete;
         Model(const std::string& path);
-        // glm::mat4 get_model_matrix();
+        ~Model();
 
     private:
         void processNode(aiNode* node, const aiScene* scene);
@@ -29,6 +29,8 @@ namespace ex
     // imp ----------------------------------------------------------------------------------------
     Model::Model(const std::string& path)
     {
+        std::cout << "model constructor called" << std::endl;
+
         Assimp::Importer import;
         const aiScene* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
@@ -39,6 +41,12 @@ namespace ex
         }
 
         processNode(scene->mRootNode, scene);
+    }
+    
+    // TODO: free the memory?
+    Model::~Model()
+    {
+        std::cout << "model destructor called" << std::endl;
     }
 
     void Model::processNode(aiNode* node, const aiScene* scene)
@@ -55,19 +63,5 @@ namespace ex
             processNode(node->mChildren[i], scene);
         }
     }
-
-    // glm::mat4 Model::get_model_matrix()
-    // {
-    //     tr.get_model_matrix();
-    //     glm::mat4 m_model = glm::mat4(1.0f);
-    //     m_model = glm::translate(m_model, tr.pos());
-    //     m_model = glm::rotate(m_model, glm::radians(tr.rot().x), glm::vec3(1.0f, 0.0f, 0.0f));
-    //     m_model = glm::rotate(m_model, glm::radians(tr.rot().y), glm::vec3(0.0f, 1.0f, 0.0f));
-    //     m_model = glm::rotate(m_model, glm::radians(tr.rot().z), glm::vec3(0.0f, 0.0f, 1.0f));
-    //     m_model = glm::scale(m_model, tr.scale());
-    //     tr.is_dirty = false;
-
-    //     return m_model;
-    // }
 
 } // namespace ex
