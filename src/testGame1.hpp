c100b27c9c7c282244938ceac73b185215887f17
exlef <50216668+exlef@users.noreply.github.com>
2024-09-10 13:50:25 +0300

    add ground

--- src/testGame1.hpp
+++ src/testGame1.hpp
@@ -17,15 +17,18 @@ class TestGame1
 {
 private:
     ex::App app = ex::App(800, 600, "test");
-    ex::Camera m_cam = ex::Camera(app.aspect_ratio());
+    ex::Camera m_cam = ex::Camera(app.aspect_ratio(), glm::vec3(0, 2, 10));
     ex::Model m_cube = ex::Model("src/res/models/cube.obj");
     ex::Model m_sphere = ex::Model("src/res/models/sphere.obj");
+    ex::Model m_ground = ex::Model("src/res/models/cube.obj");
     ex::Light m_light_manager;
     ex::Texture m_container_dif_tex = ex::Texture("src/res/textures/container2.png");
     ex::Texture m_container_spec_tex = ex::Texture("src/res/textures/container2_specular.png");
     ex::Texture m_marble_tex = ex::Texture("src/res/textures/marble.jpg");
+    ex::Texture m_wood_tex = ex::Texture("src/res/textures/wood.png");
     ex::Shader m_cube_shader = ex::Shader(m_container_dif_tex.id(), m_container_spec_tex.id(), glm::vec3(0.4f));
     ex::Shader m_sphere_shader = ex::Shader(m_marble_tex.id(), 0, glm::vec3(0.4f));
+    ex::Shader m_ground_shader = ex::Shader(m_wood_tex.id(), 0, glm::vec3(0.4f));
     FlyCam fly_cam = FlyCam(&m_cam);
 
 public:
@@ -39,6 +42,8 @@ public:
         m_light_manager.add_dir_light(glm::vec3(-45, 0, 0), 1);
 
         m_sphere.tr.pos.z = -2;
+        m_ground.tr.pos = glm::vec3(0, -1, 0);
+        m_ground.tr.scale = glm::vec3(10, 0.1f, 10);
 
         app.run();
     }
@@ -56,6 +61,8 @@ private:
         }
         
         draw(m_sphere, m_sphere_shader);
+
+        draw(m_ground, m_ground_shader);
     }
 
     void draw(ex::Model& m, ex::Shader& s)
