d842535c737fcc7933d6b120a6566ef183e1b2c0
exlef <50216668+exlef@users.noreply.github.com>
2024-09-10 13:37:24 +0300

    draw same mesh multiple times at different locations

--- src/testGame1.hpp
+++ src/testGame1.hpp
@@ -18,16 +18,14 @@ class TestGame1
 private:
     ex::App app = ex::App(800, 600, "test");
     ex::Camera m_cam = ex::Camera(app.aspect_ratio());
-    std::vector<ex::Model> m_models;
-    std::vector<ex::Shader> m_shaders;
-    // ex::Model m_cube = ex::Model("src/res/models/cube.obj");
-    // ex::Model m_sphere = ex::Model("src/res/models/sphere.obj");
+    ex::Model m_cube = ex::Model("src/res/models/cube.obj");
+    ex::Model m_sphere = ex::Model("src/res/models/sphere.obj");
     ex::Light m_light_manager;
     ex::Texture m_container_dif_tex = ex::Texture("src/res/textures/container2.png");
     ex::Texture m_container_spec_tex = ex::Texture("src/res/textures/container2_specular.png");
     ex::Texture m_marble_tex = ex::Texture("src/res/textures/marble.jpg");
-    // ex::Shader m_cube_shader = ex::Shader(m_container_dif_tex.id(), m_container_spec_tex.id(), glm::vec3(0.4f));
-    // ex::Shader m_sphere_shader = ex::Shader(m_marble_tex.id(), 0, glm::vec3(0.4f));
+    ex::Shader m_cube_shader = ex::Shader(m_container_dif_tex.id(), m_container_spec_tex.id(), glm::vec3(0.4f));
+    ex::Shader m_sphere_shader = ex::Shader(m_marble_tex.id(), 0, glm::vec3(0.4f));
     FlyCam fly_cam = FlyCam(&m_cam);
 
 public:
@@ -38,17 +36,9 @@ public:
         app.set_mouse_callback([this](float xpos, float ypos) {handle_mouse_move(xpos, ypos);});
         app.set_window_resize_callback([this](int width, int height) { handle_window_resize(width, height); });
 
-        m_models.reserve(2);
-        m_models.emplace_back("src/res/models/cube.obj");
-        m_models.emplace_back("src/res/models/sphere.obj");
-        m_shaders.reserve(2);
-        m_shaders.emplace_back(m_container_dif_tex.id(), m_container_spec_tex.id(), glm::vec3(0.4f));
-        m_shaders.emplace_back(m_marble_tex.id(), 0, glm::vec3(0.4f));
-
         m_light_manager.add_dir_light(glm::vec3(-45, 0, 0), 1);
 
-        m_models[0].tr.pos.x = -1;
-        m_models[1].tr.pos.x = 1;
+        m_sphere.tr.pos.z = -2;
 
         app.run();
     }
@@ -57,16 +47,24 @@ private:
     {
         fly_cam.move(app);
 
-        m_models[0].tr.pos.y -= sinf(glfwGetTime()) * app.dt();
-        m_models[1].tr.pos.y += sinf(glfwGetTime()) * app.dt();
+        m_sphere.tr.pos.y += sinf(glfwGetTime()) * app.dt();
 
-        update_shader(m_shaders[0], m_models[0]);
-        app.draw(m_models[0]);
-        update_shader(m_shaders[1], m_models[1]);
-        app.draw(m_models[1]);
+        for (size_t i = 0; i < 5; i++)
+        {
+            m_cube.tr.pos.x = (4 - ((int)i * 2));
+            draw(m_cube, m_cube_shader);
+        }
+        
+        draw(m_sphere, m_sphere_shader);
     }
 
-    void update_shader(ex::Shader& s, ex::Model& m)
+    void draw(ex::Model& m, ex::Shader& s)
+    {
+        update_shader(m, s);
+        app.draw(m);
+    }
+
+    void update_shader(ex::Model& m, ex::Shader& s)
     {
         s.use();
 
