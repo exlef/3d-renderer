ee35630bf77c61a781b96f5a0ef0c52191aaf37c
exlef <50216668+exlef@users.noreply.github.com>
2024-09-23 18:06:48 +0300

    fix gamma correction being applied without postprocessing

--- src/ex3d/config.hpp
+++ src/ex3d/config.hpp
@@ -6,7 +6,7 @@ namespace ex
 {
     constexpr bool SHOW_SKYBOX = true;
     constexpr bool APPLY_PP = true;
-    #define APPLY_GAMMA_CRRC true
+    constexpr bool APPLY_GAMMA_CRRC = (APPLY_PP && true);
     constexpr float GAMMA = 2.2f;
     constexpr glm::vec4 CLEAR_COLOR = glm::vec4(0.2f);
     constexpr bool HIDE_CURSOR = true;
